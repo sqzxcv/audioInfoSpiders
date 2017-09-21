9917cb8874c4097a68ae80b4b02d84adfeff59d5
Xia Jun <xiajun@XiadeMacBook.local>
2017-09-21 16:34:53 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -35,7 +35,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:sqzxcv/audioInfoSpiders.git',
       path: '/var/www/audioInfoSpiders',
-      "post-deploy": 'nvm use 8.0.0 && git pull && cnpm install && pm2 reload ecosystem.config.js --env production'
+      "post-deploy": 'nvm use 8.0.0 && git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
